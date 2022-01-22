968d124301c8fda0931268d4b60fd707d9822458
GAHYUN-P <gahyun94526@gmail.com>
2022-01-22 20:00:46 +0900

    CSS_인라인

--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -9,7 +9,17 @@ import World from "./World";
 export default function Hello() {
     return (
         <div>
-            <h1>Hello</h1>
+            <h1 style = {
+{
+    color : '#f00',  // 객체기때문에 쉼표로 구분
+    borderRight : '12px solid #000',
+    // 키는 CSS에서는 border-right라고 썼다면 여기서는 borderRight라고 쓴다
+    marginBottom : '50px',
+    opacity : 0.5,
+}
+
+            }>Hello</h1>
+            <World />
             <World />
         </div>
         // JSX는 하나의 태그만 만들 수 있기때문에 div가 없으면 에러가 난다
