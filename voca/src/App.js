83acdf7baca64e7221fe46bbaf52ac5fa9f7f72f
GAHYUN-P <gahyun94526@gmail.com>
2022-01-25 01:42:02 +0900

    state_동일한 컴포넌트더라도 state는 따로 관리된다

--- voca/src/App.js
+++ voca/src/App.js
@@ -7,6 +7,10 @@ function App() {
   return <div className="App">
     <Hello /> 
     {/* 셀프클로즈 */}
+    <Hello /> 
+    <Hello /> 
+    {/* 동일한 component더라도 state는 따로 관리된다
+    다른 state에 영향을 미치지 않는다. */}
     
   </div>
 }
--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -17,7 +17,6 @@ export default function Hello() {
 
     return (
         <div>
-            <h1>State</h1>
             <h2 id="name">{name}</h2>
             <button onClick={()=>{
                 setName(name === "Mike" ? "Jane" : "Mike");
