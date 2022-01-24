ad85fc045c3d02d6e7345d647447299d25a77c16
GAHYUN-P <gahyun94526@gmail.com>
2022-01-25 01:29:34 +0900

    state_getElementBy로 업데이트

--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -11,14 +11,15 @@ export default function Hello() {
     function changeName() {
         name = name === "Mike" ? "Jane" : "Mike";
         console.log(name);
+        document.getElementById("name").innerText = name;
+        // id name인 엘리먼트에 Text를 name으로 바꿔준다
     }
 
     return (
         <div>
             <h1>State</h1>
-            <h2>{name}</h2>
+            <h2 id="name">{name}</h2>
             <button onClick={changeName}>Change</button>
-            {/* 버튼을 눌러도 변화가 없다 */}
         </div>
         
     );
